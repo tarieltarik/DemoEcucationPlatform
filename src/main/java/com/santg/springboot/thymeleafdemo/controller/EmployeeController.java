9206e812cb72e78aaa13f7d56912692ed777f136
Santiago Grangetto <santiagograngetto@gmail.com>
2019-07-05 21:23:29 -0300

    Set index.html
    Set index.html to redirect to /employees/list

--- src/main/java/com/santg/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/santg/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,48 +1,37 @@
 package com.santg.springboot.thymeleafdemo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.santg.springboot.thymeleafdemo.entity.Employee;
+import com.santg.springboot.thymeleafdemo.service.EmployeeService;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
-	 // load movies data
-	 private List<Employee> employees;
+	private EmployeeService employeeService;
+	
+	@Autowired
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
+	
+	// add mapping for "/list"
+	@GetMapping("/list")
+	public String employeeList(Model model) {
+	
+		// get employees from data base
+		List<Employee> employees = employeeService.findAll();
+		
+		// add to the spring model
+		model.addAttribute("employees", employees);
 	 
-	 @PostConstruct
-	 private void loadData() {
-		 
-		 // create movie
-		 Employee emp1 = new Employee(1, "Liliana", "Vess", "liliana@mail.com");
-		 Employee emp2 = new Employee(2, "Jace", "Beleren", "jace@mail.com");
-		 Employee emp3 = new Employee(3, "Gideon", "Jura", "gideon@mail.com");
-		 
-		 // create list
-		 employees = new ArrayList<>();
-		 
-		 // add to list
-		 employees.add(emp1);
-		 employees.add(emp2);
-		 employees.add(emp3);
-	 }
-	 
-	 // add mapping for "/list"
-	 @GetMapping("/list")
-	 public String employeeList(Model model) {
-		 
-		 // add to the spring model
-		 model.addAttribute("employees", employees);
-		 
-		 return "list-employees";
-	 }
+		return "list-employees";
+	}
 }
