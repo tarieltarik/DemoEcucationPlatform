8064171f825b820222a9f91af53901de85bb5071
Santiago Grangetto <santiagograngetto@gmail.com>
2019-07-05 22:16:56 -0300

    Add method to sort by last name
    Add method to repository to find the list by order by last name

--- src/main/java/com/santg/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ src/main/java/com/santg/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.santg.springboot.thymeleafdemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.santg.springboot.thymeleafdemo.entity.Employee;
@@ -8,4 +10,7 @@ import com.santg.springboot.thymeleafdemo.entity.Employee;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// that's it
+	
+	// add method to sort by last name
+	public List<Employee> findAllByOrderByLastNameAsc();
 }
